83b052cca96af3bb3593a0a0218407f8ed9a563e
Alex Rudyak <59541334+AlexRudyak@users.noreply.github.com>
2022-04-19 15:51:38 +0300

    Recursive python to-do list
    Just a simple recursive python to do list, you can add, remove, view and exit.
    Probably one of the most basic things I could do.

---
+++ app.py
@@ -0,0 +1,45 @@
+user_input = "random"
+
+data = []
+
+option_list = ["Add and item",
+               "Mark as done",
+               "View items",
+               "Exit"]
+
+def show_menu(list):
+    for number,item in enumerate(list):
+        print("{}) {}".format(number+1,item))
+    print("")
+
+
+show_menu(option_list)
+user_input = int(input ("Enter your choice: "))
+
+while user_input != option_list.index(option_list[-1])+1:
+
+    if user_input == 1:
+        item = input("What item do you want to add to the to-do list ?\n")
+        data.append(item)
+        print("added",item,"to the list\n")
+    
+    elif user_input == 2:
+        print("Your list is:")
+        show_menu(data)
+        item = int(input("What number to remove from list?\n"))
+        print("Removed",data[item-1],"\n")
+        data.remove(data[item-1])
+
+    elif user_input == 3:
+        print("Your list is:")
+        show_menu(data)
+        print("")
+
+    elif user_input == 4:
+        print("Goodbye")
+
+    else:
+        print("Enter 1,2,3 or 4")
+    
+    show_menu(option_list)
+    user_input = int(input ("Enter your choice: "))
