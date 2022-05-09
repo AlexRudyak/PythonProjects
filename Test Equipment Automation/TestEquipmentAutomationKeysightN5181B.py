7f599e92ebc65331b52edd5682a39ae8d3707098
Alex Rudyak <59541334+AlexRudyak@users.noreply.github.com>
2022-05-09 13:36:50 +0300

    Added frequency input
    changed the frequency to be user input

--- Test Equipment Automation/TestEquipmentAutomationKeysightN5181B.py
+++ Test Equipment Automation/TestEquipmentAutomationKeysightN5181B.py
@@ -6,14 +6,16 @@ rm = pyvisa.ResourceManager()
 sg_com = rm.list_resources()[0]
 sg = rm.open_resource(sg_com)
 
-starting_amp = int(input("Input Starting Amplitude:"))  # -70 dBm
-stop_amp = int(input("Input Stopping Amplitude:"))  # -50 dBm
-incr = int(input("Input Increments:"))  # 2 dB
-wait_time = int(input("How many seconds between each mode? "))
+frequency = int(input("Input Frequency in GHz: "))
+starting_amp = int(input("Input Starting Amplitude: "))  # -70 dBm
+stop_amp = int(input("Input Stopping Amplitude: "))  # -50 dBm
+incr = int(input("Input Increments: "))  # 2 dB
+wait_time = int(input("How many seconds between each mode?: "))
 
 sg.write(":OUTP ON")
 
-sg.write(':FREQ:FIX 1.0 GHz')
+# L1 Band
+sg.write(":FREQ:FIX " + str(frequency) + "GHz")
 power = starting_amp
 for i in range(abs(math.floor((stop_amp - starting_amp)/incr))+1):  # -70 - (-50) / 2 = -10 == 10 |||| 50 - 40 / 1 = 10
     print("The Signal Power is: " + str(power) + "dBm")
