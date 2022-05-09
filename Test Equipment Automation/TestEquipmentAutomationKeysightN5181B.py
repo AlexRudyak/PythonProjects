d2e19b6637cd63299c198e84d3fb08db5b221c5a
Alex Rudyak <59541334+AlexRudyak@users.noreply.github.com>
2022-05-09 13:32:32 +0300

    Update TestEquipmentAutomationKeysightN5181B.py

--- Test Equipment Automation/TestEquipmentAutomationKeysightN5181B.py
+++ Test Equipment Automation/TestEquipmentAutomationKeysightN5181B.py
@@ -13,20 +13,9 @@ wait_time = int(input("How many seconds between each mode? "))
 
 sg.write(":OUTP ON")
 
-# L1 Band
 sg.write(':FREQ:FIX 1.0 GHz')
 power = starting_amp
 for i in range(abs(math.floor((stop_amp - starting_amp)/incr))+1):  # -70 - (-50) / 2 = -10 == 10 |||| 50 - 40 / 1 = 10
-    print("The Signal Power is: " + str(power) + "dBm")
-    sg.write(":POWER:AMPL " + str(power) + "dBm")
-    power += incr
-    sleep(wait_time)
-
-
-# L2 Band
-sg.write(':FREQ:FIX 3.0 GHz')
-power = starting_amp
-for i in range(abs(math.floor((stop_amp - starting_amp)/incr))+1):  # 0, 1, 2 -80 - (-50 = -30 / 7 = 28/7 = 4
     print("The Signal Power is: " + str(power) + "dBm")
     sg.write(":POWER:AMPL " + str(power) + "dBm")
     power += incr
